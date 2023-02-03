6949866fba0477c13e15ecf907e7470b4be011e6
sepehrraisi <sepehr.raisi@gmail.com>
2023-02-03 20:01:17 +0330

    Update reformatted code for better readability.

--- OCR/detect.py
+++ OCR/detect.py
@@ -65,96 +65,54 @@ def match_template(image, template):
     return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
 
 
-# filename = '3.png'
-# img = np.array(Image.open(filename))
-
-def main(inputfile, langs="faen", mode="tn"):
+def main(inputfile, langs="faen", mode="t"):
     img = cv2.imread(inputfile)
-    # img = cv2.medianBlur(img, 5)
-    # ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    # th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    # ret3,img = cv2.threshold(img,200,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # img = cv2.copyMakeBorder(img, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[255, 255, 255])
-    # table_c = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (3, 3), 0, 0)
-    # # Threshold
-    # img = table_c
-    # _, img = cv2.threshold(table_c, 180, 255, cv2.THRESH_BINARY, cv2.THRESH_OTSU)
-    # cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
-    # img = cv2.normalize(img, cv2norm_img, 0, 255, cv2.NORM_MINMAX)
-    # img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
-    # img = cv2.GaussianBlur(img, (1, 1), 0)
-
     gray = get_grayscale(img)
+    ## Different Modes for image proccessing 
+    img = gray
     # gray = deskew(gray)
     # gray = erode(gray)
     # thresh = thresholding(gray)
     # thresh = remove_noise(gray)
     # opening = opening(gray)
     # canny = canny(gray)
-    img = gray
-    # cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
-    # img = cv2.normalize(img, cv2norm_img, 0, 255, cv2.NORM_MINMAX)
-    # img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
-    # img = cv2.GaussianBlur(img, (1, 1), 0)
-    custom_config = r'-l fas --psm 6"'
+
+##Configs for Different OCR configs
+    custom_config = r'-l faen --psm 6"'
     if langs == "fa":
         if mode == "t":
             custom_config = r'-l fas --psm 6 -c tessedit_char_blacklist="۰١۲۳۴۵۶۷۸۹«»1234567890#"'
         if mode == "tn":
             custom_config = r'-l fas --psm 6 -c tessedit_char_whitelist="ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ۰١۲۳۴۵۶۷۸۹.?!,،:;/"'
         if mode == "table":
-            custom_config = r'-l fas --psm 1 -c tessedit_char_whitelist="ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ۰١۲۳۴۵۶۷۸۹"'
-
+            custom_config = r'-l fas --psm 6 -c tessedit_char_whitelist="ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ۰١۲۳۴۵۶۷۸۹"'
     elif langs == "en":
         custom_config = r'-l eng --psm 6'
     elif langs == "faen":
-        custom_config = r'-l fas+eng --psm 1 --oem 3'
+        custom_config = r'-l fas+eng --psm 6 '
     else:
-        print("Choose a valid Language.")
+        print("Choose valid Options.")
         exit(0)
-    # img = np.array(gray)
-    # for i in [1,3,4,5,6,7,8,9,10,11,12]:
-    #     custom_config = fr'-l fas --psm {i}'
+
+    ## Convert Image to Text
     text = pytesseract.image_to_string(img, config=custom_config)
-    # print(i)
-    # print(text)
+
+    ## Write Results to Result.txt with UTF-8 Encoding.
     with io.open('result.txt', 'w', encoding='utf8') as f:
         f.write(text)
 
-    # text = str(text.encode("utf-8"))
-    # # open text file
-    # text_file = open("data.txt", "w")
-    #
-    # # write string to file
-    # text_file.write(text)
-    #
-    # # close file
-    # text_file.close()
-    # return text
+## Spell Checking (STILL IN PROGRESS)
     # print(text.split())
     # spc = []
     # for word in text.split():
     #     # print(word)
     #     # print(0, spell.correction(word))
     #     spc.append(spell.correction(word))
-    # print(spc)
-    # tmp2 = spc.split(',')
-    # print(spell.correction("سلان"))
-    # text2 = pytesseract.image_to_string(thresh, config=custom_config)
-    # print(text2)
-    # text3 = pytesseract.image_to_string(opening, config=custom_config)
-    # print(text3)
-    # text4 = pytesseract.image_to_string(canny, config=custom_config)
-    # print(text4)
-    # return convert(text)
-    # print(text)
-    # cv2.imshow('image window', img)
-    # add wait key. window waits until user presses a key
-    # cv2.waitKey(0)
-    # and finally destroy/close all open windows
-    # cv2.destroyAllWindows()
 
+## Show Proccessed Image that is being used by pytesseract.
+    # cv2.imshow('image window', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
