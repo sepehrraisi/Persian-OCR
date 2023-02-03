8df6bb8ee1a9341de14933a3fd142f827dbfaf46
sepehrraisi <sepehr.raisi@gmail.com>
2023-02-03 19:50:28 +0330

    Fix Argument system.

--- ocr.py
+++ ocr.py
@@ -1,21 +1,22 @@
-import getopt
-import sys
+import sys, getopt
 from OCR import detect
 
-
 def main(argv):
-    inputfile = ''
-    try:
-        opts, args = getopt.getopt(argv, "h:i", ["ifile="])
-    except getopt.GetoptError:
-        print('test.py -i <inputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-    detect.main(inputfile)
+   inputfile = ''
+   mode = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:m:",["ifile=","mode="])
+   except getopt.GetoptError:
+      print ('test.py -i <inputfile> -m <mode>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -i <inputfile> -m <mode>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-m", "--mode"):
+         mode = arg
+   detect.main(inputfile)
 if __name__ == "__main__":
-    main(sys.argv[1:])
+   main(sys.argv[1:])
